023f33c3368251a3f726c566a8ecca9be0029945
SthepanieArroyo <60743502+SthepanieArroyo@users.noreply.github.com>
2020-02-07 21:14:57 -0300

    clase 2
    Ejercicios de entrada y salida

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -2,8 +2,11 @@
 	Debemos lograr tomar un dato por 'PROMPT' 
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
-function mostrar()
-{
+function mostrar() {
+	var nombre;
 
+	nombre = document.getElementById('elNombre');
+	nombre = prompt("Ingrese su nombre");
+	alert("Su nombre es:  " + nombre);
 }
 
